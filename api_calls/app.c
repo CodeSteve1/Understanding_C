db76b5cc1bc7f209d3d42f0a0601ae07b83bb114
CodeSteve1 <92751312+CodeSteve1@users.noreply.github.com>
2024-10-03 20:05:45 +0530

    Add files via upload

---
+++ api_calls/app.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "addition.h"
+#include "multiplication.h"
+
+int main()
+{   
+    int add_val=Addition(5,5);
+    printf("Added_value is : %d \n",add_val); 
+    int mul_val = Multiplication(5,5); 
+    printf("Multiplied Value is : %d \n",mul_val);
+    return 0;
+}
