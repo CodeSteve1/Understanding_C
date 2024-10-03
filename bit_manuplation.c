aadb5311b161e2af9b20329554736a3e7f1150e9
CodeSteve1 <92751312+CodeSteve1@users.noreply.github.com>
2024-10-03 19:37:05 +0530

    Update bit_manuplation.c

--- bit_manuplation.c
+++ bit_manuplation.c
@@ -7,9 +7,17 @@ int main()
     char arbit = reg; //setting a arbitary variable 
     set_bit(arbit,2);  //calling macro function to set variable arbit's 3rd bit value
     printf("After setting 3rd bit: %c\n", arbit);//printing arbit after setting
-
-…        printf("5th bit is not set.\n");
+    clear_bit(arbit,3); //calling macro function to clear the 4th bit of arbit
+    printf("After clearing 4th bit: %c\n", arbit);//printing arbit after clearing the 4th bit
+    if (arbit & (1 << 4))      //checking if the 5th bit is set or not
+    {
+        printf("5th bit is set.\n");
     }
+    else
+    {
+        printf("5th bit is not set.\n");
+    }
+    
 
     return 0;
 }
